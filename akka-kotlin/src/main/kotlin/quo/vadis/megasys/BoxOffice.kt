1117af3ba380943bdb89ca776b269ff13d1c803a
megasys1968 <megasys@kzd.biglobe.ne.jp>
2019-11-21 23:16:53 +0900

    途中

--- akka-kotlin/src/main/kotlin/quo/vadis/megasys/BoxOffice.kt
+++ akka-kotlin/src/main/kotlin/quo/vadis/megasys/BoxOffice.kt
@@ -1,39 +1,126 @@
 package quo.vadis.megasys
 
 import akka.actor.AbstractLoggingActor
+import akka.actor.ActorRef
+import akka.actor.Props
 import java.time.Duration
+import java.util.*
+import java.util.concurrent.CompletableFuture
+import akka.pattern.PatternsCS.ask
+import akka.pattern.PatternsCS.pipe
+import java.util.concurrent.CompletionStage
+import java.util.stream.Collectors
+import java.util.stream.IntStream
 
 class BoxOffice(val timeout: Duration): AbstractLoggingActor() {
-
   companion object {
-    data class CreateEvent(val name: String, val tickets: Int)
-    data class GetEvent(val name: String)
-    object GetEvents
-    data class GetTickets(val event: String, val tickets: Int)
-    data class CancelEvent(val name: String)
+    fun props(timeout: Duration): Props {
+      return Props.create(BoxOffice::class.java) { BoxOffice(timeout) }
+    }
+  }
 
-    interface EventResponse
-    data class Event(val name: String, val tickets: Int): EventResponse
-    data class Events(val events: List<Event>): EventResponse
+  data class CreateEvent(val name: String, val tickets: Int)
+  data class GetEvent(val name: String)
+  object GetEvents
+  data class GetTickets(val event: String, val tickets: Int)
+  data class CancelEvent(val name: String)
+
+  data class Event(val name: String, val tickets: Int)
+  data class Events(val events: List<Event>)
+
+  interface EventResponse
+  data class EventCreated(val event: Event): EventResponse
+  object EventExists : EventResponse
+
+
+  private val msg = "    📩 {}"
+
+
+  private fun createTicketSeller(name: String): ActorRef {
+    return context.actorOf(TicketSeller.props(name), name)
   }
 
   override fun createReceive(): Receive =
     receiveBuilder()
-      .match(CreateEvent::class.java) {
+      .match(CreateEvent::class.java) { createEvent->
+        log().debug(msg, createEvent)
+
+        context.findChild(createEvent.name)
+          .ifPresentOrElse({
+
+          getContext().sender().tell( EventExists, self());
+        } , {
+          val eventTickets = createTicketSeller(createEvent.name)
+          val newTickets =
+          IntStream.rangeClosed(1, createEvent.tickets)
+            .mapToObj{ TicketSeller.Ticket(it) }
+          .collect(Collectors.toList());
+
+          eventTickets.tell( TicketSeller.Add(newTickets), self);
+          getContext().sender().tell(EventCreated(Event(createEvent.name, createEvent.tickets)), self);
+        })
+
 
       }
-      .match(GetTickets::class.java) {
+      .match(GetTickets::class.java) { getTickets ->
+        log().debug(msg, getTickets)
+
+        context.findChild(getTickets.event)
+          .ifPresentOrElse({
+            it.forward(TicketSeller.Buy(getTickets.tickets), context)
+          }, {
+            context.sender().tell(TicketSeller.Tickets(getTickets.event), self)
+          }        )
+      }
+      .match(GetEvent::class.java) { getEvent ->
+        log().debug(msg, getEvent)
+
+        context.findChild(getEvent.name)
+          .ifPresentOrElse({
+            it.forward(TicketSeller.GetEvent, context)
+          }, {
+            context.sender().tell(Optional.empty<Any>(), self)
+          }        )
+      }
+      .match(GetEvents::class.java) {getEvents->
+        log().debug(msg, getEvents)
+
+        // 子アクター（TicketSeller）に ask した結果のリストを作成
+        val children = mutableListOf<CompletableFuture<Optional<Event>>>()
+
+        context.children.forEach{child ->
+        children.add(ask(self, GetEvent(child.path().name()), timeout)
+          .thenApply{ it as Optional<Event> }
+          .toCompletableFuture())
+        }
+
+        // List<CompletableFuture<Optional<Event>>> の children を CompletionStage<Events> に変換
+        // Events は List<Event> を持つ
+        val futureEvents = CompletableFuture
+          .allOf(*children.toTypedArray())
+          .thenApply{_ ->
+        children.stream()
+          .map{ it.join() }
+          .filter{ it.isPresent }
+          .map{ it.get() }
+          .collect(Collectors.toList())
+          .let { Events(it) }
+      }
+
+        pipe(futureEvents, context.dispatcher()).to(sender());
 
       }
-      .match(GetEvent::class.java) {
+      .match(CancelEvent::class.java) {cancelEvent->
+        log().debug(msg, cancelEvent)
 
-      }
-      .match(GetEvents::class.java) {
-
-      }
-      .match(CancelEvent::class.java) {
+        context.findChild(cancelEvent.name)
+          .ifPresentOrElse({
+            it.forward(TicketSeller.Cancel, context);
+          },
+            {
+              context.sender().tell(Optional.empty<Any>(), self);
+            })
 
       }
       .build()
-
 }
--- akka-kotlin/src/main/kotlin/quo/vadis/megasys/TicketSeller.kt
+++ akka-kotlin/src/main/kotlin/quo/vadis/megasys/TicketSeller.kt
@@ -1,32 +1,59 @@
 package quo.vadis.megasys
 
 import akka.actor.AbstractLoggingActor
+import akka.actor.PoisonPill
+import akka.actor.Props
 
-class TicketSeller: AbstractLoggingActor() {
+
+class TicketSeller(private val event: String) : AbstractLoggingActor() {
   companion object {
-    data class Add(val tickets: List<Ticket>)
-    data class Buy(val tickets: Int)
-    data class Ticket(val id: Int)
-    data class Tickets(val event: String,
-                   val entries: MutableList<Ticket> = mutableListOf())
-    object GetEvent
-    object Cancel
+    fun props(event: String): Props {
+      return Props.create(TicketSeller::class.java) { TicketSeller(event) }
+    }
   }
 
+  data class Add(val tickets: List<Ticket>)
+  data class Buy(val tickets: Int)
+  data class Ticket(val id: Int)
+  data class Tickets(val event: String,
+                     val entries: MutableList<Ticket> = mutableListOf())
+
+  object GetEvent
+  object Cancel
+
+  private val msg = "    📩 {}";
+  private val tickets = mutableListOf<Ticket>()
 
   override fun createReceive(): Receive =
     receiveBuilder()
       .match(Add::class.java) {
-
+        log().debug(msg, it)
+        tickets.addAll(it.tickets)
       }
       .match(Buy::class.java) {
+        log().debug(msg, it)
+
+        if (tickets.size >= it.tickets) {
+          val entries = tickets.subList(0, it.tickets)
+          context.sender().tell(Tickets(event, entries), self);
+          entries.clear();
+        } else {
+          context.sender().tell(Tickets(event), self);
+        }
 
       }
       .match(GetEvent::class.java) {
+        log().debug(msg, it)
+
+        sender().tell(BoxOffice.Event(event, tickets.size), self);
 
       }
       .match(Cancel::class.java) {
+        log().debug(msg, it)
+
+        sender().tell(BoxOffice.Event(event, tickets.size), self);
+        self().tell(PoisonPill.getInstance(), self());
 
       }
-      .build();
+      .build()
 }
